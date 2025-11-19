cf69b920471bfa9fa1d8e7d221fc8f9356c3efd2
anton2026gamca <Abraham1@gamca.sk>
2025-11-19 18:30:36 +0100

    Add meteoroid look & size randomization

--- src/world/meteoroid/meteoroid.gd
+++ src/world/meteoroid/meteoroid.gd
@@ -2,9 +2,40 @@ extends RigidBody2D
 class_name Meteoroid
 
 
-@export var lines: Array[Line2D]
-
 var health: float = 500
+
+@onready var lines: Array[Line2D] = [$Line1, $Line2, $Line3, $Line4, $Line5, $Line6, $Line7]
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+@export var random_size_modifier_min: float = 0.8
+@export var random_size_modifier_max: float = 1.3
+
+
+
+func _ready() -> void:
+	if collision_shape.shape is CircleShape2D:
+		collision_shape.shape = collision_shape.shape.duplicate()
+		collision_shape.shape.radius *= randf_range(random_size_modifier_min, random_size_modifier_max)
+	var angles: Array[float] = []
+	var radius_modifiers: Array[float] = []
+	var count: int = len(lines)
+	var val: float = 0
+	angles.append(0)
+	radius_modifiers.append(1)
+	for i: float in count - 1:
+		var step: float = deg_to_rad(randf_range((1.0 / count) * 360 - 10, (1.0 / count) * 360) + 10)
+		val += step
+		angles.append(val)
+		radius_modifiers.append(randf_range(0.7, 1.3))
+	angles.append(0)
+	radius_modifiers.append(1)
+	for i: float in count:
+		var radius: float = 1
+		if collision_shape.shape is CircleShape2D:
+			radius = collision_shape.shape.radius
+		lines[i].points[0] = Vector2.from_angle(angles[i]) * radius * radius_modifiers[i]
+		lines[i].points[1] = Vector2.from_angle(angles[i + 1]) * radius * radius_modifiers[i + 1]
+		print(angles[i], " -> ", angles[i + 1])
 
 
 func damage(val: float) -> void:
